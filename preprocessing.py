ee79e1f5fa97c580acd4a087179c0eb06589f3a5
Denis Mazur <denis.mazur.02@inbox.ru>
2017-07-06 23:02:21 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,11 @@
-import os
 import numpy as np
 from PIL import Image
 from PIL import ImageOps
 
 def jpg_to_nparray(path,img_names, img_size, grayscale = False):
     X = []
-    img_colors = 3
 
-    for counter,img_dir in enumerate(img_names):
+    for img_dir in img_names:
         img = Image.open(path+img_dir)
         img = ImageOps.fit(img, img_size, Image.ANTIALIAS)
         
@@ -16,8 +14,7 @@ def jpg_to_nparray(path,img_names, img_size, grayscale = False):
             img_colors = 1
 
         img = np.asarray(img, dtype = 'float32') / 255.
-        #0 1 2
-        img = np.transpose(img,[2,0,1])#.reshape([img_colors]+list(img_size))
+        img = np.transpose(img, [2, 0, 1])
         X.append(img)
 
     X = np.asarray(X)
